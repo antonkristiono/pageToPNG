de821be8c2fff79d5806a5242fb115ab366a1c76
Anton Kristiono <antonkristiono47@gmail.com>
2021-06-29 14:02:55 +0700

    height width custom

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 const puppeteer = require ('puppeteer');
-const url = "https://www.youtube.com";
+const url = "https://n-card.vercel.app/cetak/itsnu/front/guru";
 
 const Screenshot = async () => {
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
+
+  
+  await page.setViewport({
+    width: 1684,
+    height: 2384
+  })
+
   await page.screenshot({
     path: "./hasil/screenshot.png",
     fullPage: true
   });
-
   await page.close();
   await browser.close();
 }
