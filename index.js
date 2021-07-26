b6812b60e9b0c696b6ff125c46006b30ce1fa05f
Anton Kristiono <antonkristiono47@gmail.com>
2021-07-26 13:40:24 +0700

    tambah settingan

--- index.js
+++ index.js
@@ -2,21 +2,40 @@ const puppeteer = require ('puppeteer');
 const url = "http://localhost:3000/cetak/nj/front";
 
 const Screenshot = async () => {
+  // const browser = await puppeteer.launch({defaultViewport: null});
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   await page.goto(url);
 
   
-  await page.setViewport({
-    width: 2000,
-    height: 5000,
-    deviceScaleFactor: 3
-  })
+  // await page.setViewport({
+  //   height: 3178,
+  //   width: 2245,
+  //   deviceScaleFactor: 2
+  // })
 
-  await page.screenshot({
-    path: "../cropperBulk/target/screenshot.png",
-    fullPage: true
+  // await page.screenshot({
+  //   path: "../CropperBulk/target/screenshot.png",
+  //   fullPage: true
+  // });
+
+  await page.pdf({ 
+    path: 'TES.pdf', 
+    format: 'A1',
+    printBackground: true,
+    scale: 0.69,
+    omitBackground: true,
+    // margin : {
+    //   top: 1,
+    //   bottom: 1,
+    //   left: 1,
+    //   right: 1
+    // },
+    width: 7016,
+    height: 9933,
   });
+
+  
   await page.close();
   await browser.close();
 }
